c20570f02ead949cae1e003f66c32efe0e8cfd33
Niels284 <40193974@roctilburg.nl>
2022-09-22 20:24:25 +0200

    updated on 22092022

--- klachtenformulier/index.php
+++ klachtenformulier/index.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+if (array_key_exists('error_reporting', $_SESSION)) {
+    echo '<script>alert("' . $_SESSION['error_reporting']['status'] . ' | ' . $_SESSION['error_reporting']['message'] . '")</script>';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="nl">
 
@@ -13,14 +25,38 @@
     <main>
         <h1>Klachtenformulier</h1>
         <div class="formDiv">
-            <form method="POST" action="../src/process.php">
-                <label for="name">Name:</label>
-                <input type="text" name="name" required>
-                <label for="emailadres">Emailadres:</label>
-                <input type="text" name="emailadres" required>
-                <label for="describtion">Describtion:</label>
-                <textarea name="describtion" fixed size></textarea>
-                <button type="submit" name="submit">submit</button>
+            <form method="POST" action="./process.php">
+                <?php
+                if (array_key_exists('formData', $_SESSION) && !empty($_SESSION['formData'])) {
+                    echo '
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" value="' . $_SESSION['formData']['name'] . '" required>
+                    <label for="emailaddress">emailaddress:</label>
+                    <input type="text" name="emailaddress" value="' . $_SESSION['formData']['emailaddress'] . '" required>
+                    <label for="description">Description:</label>
+                    <textarea name="description" fixed size>' . $_SESSION['formData']['description'] . '</textarea>
+                    ';
+                } else {
+                    echo '
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" required>
+                    <label for="emailaddress">emailaddress:</label>
+                    <input type="text" name="emailaddress" required>
+                    <label for="description">Description:</label>
+                    <textarea name="description" fixed size></textarea>
+                    ';
+                }
+                ?>
+                <div class="buttonSection">
+                    <button type="submit" name="submit">submit</button>
+                    <?php
+                    if (file_exists('./info.log')) {
+                        echo '<a href="./info.log" target="_blank">view logs</a>';
+                    } else {
+                        echo '<script>console.log("\"info.log\" file not found!");</script>';
+                    }
+                    ?>
+                </div>
             </form>
         </div>
     </main>
---
+++ klachtenformulier/process.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+if (!array_key_exists('error_reporting', $_SESSION)) {
+    $_SESSION['error_reporting'] = [];
+}
+
+if (!array_key_exists('formData', $_SESSION) || array_key_exists('formData', $_SESSION)) {
+    $_SESSION['formData'] = [];
+}
+
+if (isset($_POST['submit'])) {
+    $_SESSION['formData'] = array(
+        "name" => $_POST['name'],
+        "emailaddress" => $_POST['emailaddress'],
+        "description" => $_POST['description']
+    );
+}
+
+require_once "../src/logs/vendor/autoload.php";
+require_once '../src/phpmailer/vendor/autoload.php';
+
+//phpMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//monolog
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['name']) && !empty($_POST['emailaddress']) && !empty($_POST['description'])) {
+        $name = htmlspecialchars(strip_tags($_POST['name']));
+        $emailaddress = htmlspecialchars(strip_tags($_POST['emailaddress']));
+        $description = htmlspecialchars(strip_tags($_POST['description']));
+
+        // constants
+        $username = "";
+        $homepage = "location:../klachtenformulier/index.php";
+
+        $mail = new PHPMailer(true);
+        try {
+
+            //Sends mail to client
+            //Server settings
+            $mail->SMTPDebug = 0;
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = $username;
+            $mail->Password   = '';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port       = 465;
+
+            //Recipients 
+            $mail->setFrom($senderMailAddress);
+            $mail->addAddress($emailaddress, $name);
+            $mail->addCC($username);
+
+            //Content
+            $mail->isHTML(true);
+            $mail->Subject = 'Uw klacht is in behandeling';
+            $mail->Body    = $description;
+
+            $mail->send();
+
+            // logs userdata
+            $logger = new Logger('info');
+            $logger->pushHandler(new StreamHandler(__DIR__ . '/info.log', Level::Debug));
+            $logger->info('user data:', ['name' => $name, 'emailaddress' => $emailaddress, 'description' => $description]);
+
+            $_SESSION['error_reporting'] = array("status" => "succesfull", "message" => "Message has been sent");
+            header($homepage);
+        } catch (Exception $e) {
+            $_SESSION['error_reporting'] = array("status" => "failed", "message" => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+            header($homepage);
+        }
+    }
+} else {
+    $_SESSION['error_reporting'] = array("status" => "failed", "message" => "Message could not be sent!");
+    header($homepage);
+}
